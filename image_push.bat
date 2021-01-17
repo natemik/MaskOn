114233c484f585f7a85e1109d6eeae2de788e0bf
Jehanzeb Mirza <38806743+AssortedFantasy@users.noreply.github.com>
2021-01-17 03:06:49 -0700

    Update image_push.bat

--- image_push.bat
+++ image_push.bat
@@ -1,3 +1,3 @@
-git add docs/_posts/* docs/assets/*
-git commit -m "Added new post"
-git push origin main
+git add docs/_posts/* docs/assets/* 1>>problems.txt 2>>problems2.txt
+git commit -m "Added new post" 1>>problems.txt 2>>problems2.txt
+git push origin main 1>>problems.txt 2>>problems2.txt
